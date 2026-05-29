fa7396c14af20120a0d92ba002479695b6a6bfcd
kris-189 <k.le@mailbox.org>
2026-05-29 09:31:47 +0200

    update Serenity set-up before each test

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,6 @@
 package tests;
 
-import net.serenitybdd.rest.SerenityRest;
+import io.restassured.http.ContentType;
 import net.thucydides.model.environment.SystemEnvironmentVariables;
 import net.thucydides.model.util.EnvironmentVariables;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,12 +10,10 @@ public class BaseTest {
     void setUp() {
         EnvironmentVariables env = SystemEnvironmentVariables.createEnvironmentVariables();
         String baseURI = env.getProperty("restassured.baseURI");
-        SerenityRest.setDefaultBasePath("");
-        io.restassured.RestAssured.baseURI = baseURI;
 
         io.restassured.RestAssured.requestSpecification = new io.restassured.builder.RequestSpecBuilder()
-                .addHeader("Accept", "application/json")
-                .addHeader("Content-Type", "application/json")
+                .setContentType(ContentType.JSON)
+                .setBaseUri(baseURI)
                 .build();
     }
 }
